291b600226ff2d4ae4715f90b7875fbd1cb6e836
Sarah Mounho <sarahmounho@gmail.com>
2020-10-10 18:12:51 -0500

    potentiometer arduino code

---
+++ potentiometer.ino
@@ -0,0 +1,18 @@
+/*
+  FGFS Slide Potentiometer Input
+  Reads an analog input on pin A0, prints the result
+  to the serial port
+ 
+  This example code is in the public domain.
+
+  https://playground.arduino.cc/Main/FlightGearInputPot/
+ */
+ 
+void setup() {
+  Serial.begin(9600);
+}
+ 
+void loop() {
+  int sensorValue = analogRead(A0);
+  Serial.println(sensorValue);
+}
