88f6f43573d3200e90344a98c5f26d1cf2bf0cc2
Sarah Mounho <sarahmounho@gmail.com>
2020-10-16 18:31:59 -0500

    Final bread board test (still needs graph functions)

--- main/main.ino
+++ main/main.ino
@@ -25,6 +25,11 @@ const int exam_led_red = A3; // A3 for exam start/in progress/stop LED
 const int exam_led_blue = A4; // A4 for exam start/in progress/stop LED
 const int exam_led_green = A5; // A5 for exam start/in progress/stop LED
 const int potentiometer = A1; // A1 for potentiometer
+const int relay1 = 12; // GPIO 12 for linear actuator
+const int relay2 = 13; // GPIO 13 for linear actuator
+const int enable = 11; // GPIO 11 for linear actuator enable
+const int FSR = 0; // GPIO 0 for FSR (pressure sensor)
+
 
 #define TFT_CS        10
 #define TFT_RST        9 // Or set to -1 and connect to Arduino RESET pin
@@ -36,6 +41,9 @@ int exam_flag = 0; // start/in progress/stop LED
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 int xPos = 0; //starting graph position
 
+// Linear actuator set up
+int exam_len = 0; 
+
 
 void setup(){
     // pinMode definitions
@@ -45,12 +53,18 @@ void setup(){
     pinMode(exam_led_blue, OUTPUT); // define exam LED as OUTPUT
     pinMode(exam_led_green, OUTPUT); // define exam LED as OUTPUT
     pinMode(potentiometer, INPUT); // define potentiometer pin as INPUT
+    pinMode(relay1, OUTPUT); // define relay1 as OUTPUT
+    pinMode(relay2, OUTPUT); // define relay2 as OUTPUT
+    pinMode(enable, OUTPUT); // define enable pin as OUTPUT
 
     // initial writes
     digitalWrite(on_led, LOW); // turn LED off to start
     digitalWrite(exam_led_red, HIGH); // turn LED off to start
     digitalWrite(exam_led_blue, LOW); // turn LED off to start
     digitalWrite(exam_led_green, LOW); // turn LED off to start
+    digitalWrite(enable, HIGH); // turn enable HIGH for linear actuator
+    digitalWrite(relay1, LOW); // turn off relay for linear actuator
+    digitalWrite(relay2, LOW); // turn off relay for linear actuator
 
     // potentiometer
     Serial.begin(9600);
@@ -59,8 +73,12 @@ void setup(){
     tft.initR(INITR_BLACKTAB); // Init ST7735S chip, black tab
     // large block of text
     tft.fillScreen(ST77XX_BLACK);
-    testdrawtext("Pain level during duration of exam", ST77XX_WHITE);
+    //testdrawtext("Pain level during duration of exam", ST77XX_WHITE);
     delay(1000);
+
+    // Retract linear actuator to start
+    retractActuator();
+    delay(2500);
 
 }
 
@@ -87,11 +105,21 @@ void loop() {
     
     if (exam_flag==1){
         int pain = analogRead(potentiometer); // read pain level
+        Serial.print("Pain:"); 
         Serial.println(pain); // print pain level on screen
 
         // Graph on LCD
-        int graphHeight = map(pain,0,1023,0,tft.height());
-        tft.drawPixel(xPos, tft.height() - graphHeight, ST77XX_WHITE);
+        //int graphHeight = map(pain,0,1023,0,tft.height());
+        //tft.drawPixel(xPos, tft.height() - graphHeight, ST77XX_WHITE);
+
+        // FSR
+        fsr_exam(); // Helper function
+
+        // Extend for 5 seconds 
+        // Pause for 10 seconds
+        // Retract for 5 seconds
+        // Extend/Retract actuator
+        actuator_exam(); // Helper function
 
         if (xPos >= 160) {
             // Restart, ran out of screen space
@@ -103,7 +131,7 @@ void loop() {
         else {
             xPos++; // move on to next position
         }
-        delay(1000); // delay so a reasonable amount of values viewed
+        delay(100); // delay so a reasonable amount of values viewed
         }
     delay(10); // .1 second wait to stabalize
 }
@@ -124,3 +152,76 @@ void testdrawtext(char *text, uint16_t color) {
   tft.print(text);
 }
 
+//Helper functions
+void extendActuator() {
+    digitalWrite(relay1, HIGH);
+    digitalWrite(relay2, LOW);
+}
+
+void retractActuator() {
+    digitalWrite(relay1, LOW);
+    digitalWrite(relay2, HIGH);
+}
+
+void stopActuator() {
+    digitalWrite(relay1, LOW);
+    digitalWrite(relay2, LOW);
+}
+
+void actuator_exam(){
+    // print exam_len for dev
+    Serial.print("exam_len = ");
+    Serial.println(exam_len);
+
+    if (exam_len <= 5){
+        extendActuator();
+        delay(500); // extend for 0.5 seconds
+        exam_len++; // index the exam button 
+    }
+    else if (exam_len <= 15){
+        stopActuator();
+        delay(500); 
+        exam_len++; // index the exam button
+    }
+    else if (exam_len <= 20){
+        retractActuator();
+        delay(500);
+        exam_len++; // index the exam button
+    }
+    else if (exam_len > 20){
+        exam_flag = 0; // exam over
+        exam_len = 0; // reset exam timer
+        RGB_color(LOW, HIGH, LOW); // Green exam LED
+
+    }
+}
+    
+    void fsr_exam(){
+        int fsrReading = analogRead(FSR); // read pressure level
+        Serial.println("Analog reading = ");
+        // analog voltage reading ranges from about 0 to 1023 which maps to 0V to 3.3V (= 3300 mV)
+        int fsrVoltage = map(fsrReading, 0, 1023, 0, 3300); 
+        unsigned long fsrResistance = 3300 - fsrVoltage; // fsrVoltage in mV
+        fsrResistance *= 10000; // 10K resistor
+        fsrResistance /= fsrVoltage; 
+        unsigned long fsrConductance = 1000000; // measured in microhms
+        fsrConductance /= fsrResistance; 
+
+        // Use the two FSR guide graphs to approximate the force
+        if (fsrConductance <= 1000) {
+            int fsrForce = fsrConductance / 80;
+            Serial.print("Force in Newtons: ");
+            Serial.println(fsrForce);     
+            tft.fillScreen(ST77XX_BLACK);
+            tft.print(fsrForce, ST77XX_WHITE); 
+        } else {
+            int fsrForce = fsrConductance - 1000;
+            fsrForce /= 30;
+            Serial.print("Force in Newtons: ");
+            Serial.println(fsrForce);
+            tft.fillScreen(ST77XX_BLACK);
+            tft.print(fsrForce, ST77XX_WHITE);
+        }
+        
+        
+    }
