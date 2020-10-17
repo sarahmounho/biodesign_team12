c48460b2bcbd4a36ffb2a876a0ef12e4e631edc8
Sarah Mounho <sarahmounho@gmail.com>
2020-10-17 12:50:24 -0500

    Rolling average for slide pot + graphing

--- main/main.ino
+++ main/main.ino
@@ -44,6 +44,12 @@ int xPos = 0; //starting graph position
 // Linear actuator set up
 int exam_len = 0; 
 
+// Smoothing for slide potentiometer
+const int numReadingsPain = 1000;
+int readingsPain[numReadingsPain]; // array to store readings from the analog INPUT
+int readIdxPain = 0; // idx of current reading
+int totalPain = 0; // running total
+int avgPain = 0; // the average
 
 void setup(){
     // pinMode definitions
@@ -80,6 +86,11 @@ void setup(){
     retractActuator();
     delay(2500);
 
+    // initialize all the readings to 0 for pain:
+    for (int thisReadingPain = 0; thisReadingPain < numReadingsPain; thisReadingPain++) {
+        readingsPain[thisReadingPain] = 0;
+  }
+
 }
 
 void loop() {
@@ -104,13 +115,9 @@ void loop() {
     // Need to add some sort of 'time element'
     
     if (exam_flag==1){
-        int pain = analogRead(potentiometer); // read pain level
-        Serial.print("Pain:"); 
-        Serial.println(pain); // print pain level on screen
-
-        // Graph on LCD
-        //int graphHeight = map(pain,0,1023,0,tft.height());
-        //tft.drawPixel(xPos, tft.height() - graphHeight, ST77XX_WHITE);
+        
+        // potentiometer + graph
+        pain_smooth(); // Helper function
 
         // FSR
         fsr_exam(); // Helper function
@@ -120,19 +127,7 @@ void loop() {
         // Retract for 5 seconds
         // Extend/Retract actuator
         actuator_exam(); // Helper function
-
-        if (xPos >= 160) {
-            // Restart, ran out of screen space
-            xPos = 0;
-            tft.fillScreen(ST77XX_BLACK);
-            testdrawtext("Pain level during duration of exam", ST77XX_WHITE);
-        } 
-
-        else {
-            xPos++; // move on to next position
-        }
-        delay(100); // delay so a reasonable amount of values viewed
-        }
+    }
     delay(10); // .1 second wait to stabalize
 }
 
@@ -196,32 +191,68 @@ void actuator_exam(){
     }
 }
     
-    void fsr_exam(){
-        int fsrReading = analogRead(FSR); // read pressure level
-        Serial.println("Analog reading = ");
-        // analog voltage reading ranges from about 0 to 1023 which maps to 0V to 3.3V (= 3300 mV)
-        int fsrVoltage = map(fsrReading, 0, 1023, 0, 3300); 
-        unsigned long fsrResistance = 3300 - fsrVoltage; // fsrVoltage in mV
-        fsrResistance *= 10000; // 10K resistor
-        fsrResistance /= fsrVoltage; 
-        unsigned long fsrConductance = 1000000; // measured in microhms
-        fsrConductance /= fsrResistance; 
+void fsr_exam(){
+    int fsrReading = analogRead(FSR); // read pressure level
+    Serial.println("Analog reading = ");
+    // analog voltage reading ranges from about 0 to 1023 which maps to 0V to 3.3V (= 3300 mV)
+    int fsrVoltage = map(fsrReading, 0, 1023, 0, 3300); 
+    unsigned long fsrResistance = 3300 - fsrVoltage; // fsrVoltage in mV
+    fsrResistance *= 10000; // 10K resistor
+    fsrResistance /= fsrVoltage; 
+    unsigned long fsrConductance = 1000000; // measured in microhms
+    fsrConductance /= fsrResistance; 
 
-        // Use the two FSR guide graphs to approximate the force
-        if (fsrConductance <= 1000) {
-            int fsrForce = fsrConductance / 80;
-            Serial.print("Force in Newtons: ");
-            Serial.println(fsrForce);     
-            tft.fillScreen(ST77XX_BLACK);
-            tft.print(fsrForce, ST77XX_WHITE); 
-        } else {
-            int fsrForce = fsrConductance - 1000;
-            fsrForce /= 30;
-            Serial.print("Force in Newtons: ");
-            Serial.println(fsrForce);
-            tft.fillScreen(ST77XX_BLACK);
-            tft.print(fsrForce, ST77XX_WHITE);
-        }
-        
-        
+    // Use the two FSR guide graphs to approximate the force
+    if (fsrConductance <= 1000) {
+        int fsrForce = fsrConductance / 80;
+        Serial.print("Force in Newtons: ");
+        Serial.println(fsrForce);     
+        tft.fillScreen(ST77XX_BLACK);
+        tft.print(fsrForce, ST77XX_WHITE); 
+    } else {
+        int fsrForce = fsrConductance - 1000;
+        fsrForce /= 30;
+        Serial.print("Force in Newtons: ");
+        Serial.println(fsrForce);
+        tft.fillScreen(ST77XX_BLACK);
+        tft.print(fsrForce, ST77XX_WHITE);
+    }        
+}
+
+void pain_smooth(){
+    // rolling average (currently)...may just change to pure average
+
+    // subtract the last reading:
+    totalPain = totalPain - readingsPain[readIdxPain];
+    // read from the slide pot:
+    readingsPain[readIdxPain] = analogRead(potentiometer);
+    // add the reading to the total:
+    totalPain = totalPain + readingsPain[readIdxPain]; 
+    // add to idx 
+    readIdxPain ++;
+
+    // at end of array
+    if (readIdxPain >= numReadingsPain) {
+        // Restart
+        readIdxPain = 0; 
     }
+
+    // calc avgPain:
+    avgPain = totalPain / numReadingsPain; 
+
+    Serial.print("Pain:"); 
+    Serial.println(avgPain); // print pain level on screen
+
+    // Graph on LCD
+    int graphHeight = map(avgPain,0,1023,0,tft.height());
+    tft.drawPixel(xPos, tft.height() - graphHeight, ST77XX_WHITE);
+    if (xPos >= 160) {
+        // Restart, ran out of screen space
+        xPos = 0;
+        tft.fillScreen(ST77XX_BLACK);
+        testdrawtext("Pain level during duration of exam", ST77XX_WHITE);
+    } 
+    else {
+        xPos++; // move on to next position
+    }
+}
